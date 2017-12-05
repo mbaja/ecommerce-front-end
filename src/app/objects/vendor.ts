7efc31ac994ff58a5596c97f19562ad58c00a22f
mbaja <mbaja@cs.stonybrook.edu>
2017-12-04 19:38:12 -0500

    Vendor Registration adjusted for objects n stuff

---
+++ src/app/objects/vendor.ts
@@ -0,0 +1,16 @@
+export class Vendor {
+  userid: string;       // VARCHAR(50) NOT NULL
+  first_name: string;   // VARCHAR(50) NULL,
+  last_name: string;     // VARCHAR(50) NULL,
+  email: string;         // VARCHAR(50) NOT NULL COMMENT 'Will be used as the username for logging in   // DATE NULL
+  password: string;     // VARCHAR(100) NOT NULL COMMENT 'Contains an encrypted password of the customer
+  type: string; // VARCHAR(10) NULL COMMENT 'Either \'Admin\', \'Customer\', or \'Vendor\''
+  phone: string; // VARCHAR(50) NULL
+  address: string;       // VARCHAR(50) NULL
+  city: string;         // VARCHAR(50) NULL
+  state: string;        // CHAR(2) NULL
+  country:string;       // VARCHAR(50) NULL,
+  zip: number;          // INT NULL
+  store_name:string;
+  store_desc:string;
+}
