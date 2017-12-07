3cbcc533a9fcd238de0cd9ee49a98bd73faf3a60
Arslan Sadiq <arslanmsadiq@gmail.com>
2017-12-07 18:40:05 -0500

    Vendor can now view their inventory and delete from inventory

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-store',
@@ -7,11 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreComponent implements OnInit {
 
-  vendor_item;
+  vendor_items;
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   ngOnInit() {
+
+    this.http.get('http://localhost:3000/inventory', { headers : 
+    new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }), withCredentials: true }).subscribe(data => {
+      console.log("Inventory data", data);
+      this.vendor_items = data;
+    }, (err: HttpErrorResponse) => {
+      if (err.error instanceof Error) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.log('An error occurred:', err.error.message);
+        
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+        
+      }
+    }); 
+
   }
 
   selectStoreInventoryTab(e){
@@ -36,8 +58,40 @@ export class StoreComponent implements OnInit {
   	document.getElementById("additemcontent").className = "tab-pane fade in active";  	
   }
 
-  deleteInventoryItem() {
+  deleteInventoryItem(ItemID) {
   	console.log("Clicked delte inv item");
-  	
+
+
+  	this.http.delete('http://localhost:3000/items/' + ItemID, { headers : 
+    new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }), withCredentials: true }).subscribe(data => {
+      console.log("Delete Item data", data);
+
+      var index = -1;
+
+      for (var i = 0; i < this.vendor_items.length; i++) {
+        if (this.vendor_items[i].Item.ItemID === ItemID)  {
+          index = i;
+        }
+      }
+
+      if (index >= 0)  {
+        this.vendor_items.splice(index, 1);
+      }
+
+    }, (err: HttpErrorResponse) => {
+      if (err.error instanceof Error) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.log('An error occurred:', err.error.message);
+        
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+        
+      }
+    }); 
   }
 }
